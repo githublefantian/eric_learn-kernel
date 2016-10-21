66f325d371738e9778e30166c27afd16a028d377
root <root@localhost.localdomain>
2016-10-21 00:00:20 -0400

    删除编译过程产生的文件2

--- 17-synchronize_rcu/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/mengxiao/eric/module_1/built-in.o :=  rm -f /root/mengxiao/eric/module_1/built-in.o; ar rcsD /root/mengxiao/eric/module_1/built-in.o
--- 17-synchronize_rcu/.tmp_versions/tmain.mod
+++
@@ -1,2 +0,0 @@
-/root/mengxiao/eric/module_1/tmain.ko
-/root/mengxiao/eric/module_1/main.o /root/mengxiao/eric/module_1/src/other.o
